f2cc49e23bfa36953571d6fd23e3c94de8f10b50
luciomelis <luciomelis1992@gmail.com>
2022-03-22 17:55:27 +0100

    finish jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -3,3 +3,13 @@ console.log('Js OK!');
 // Scrivi una funzione
 // che accetti una stringa come argomento
 // e la ritorni girata (es. Ciao -> oaiC)
+
+const ritornoStringa = stringaGirata('Lucio');
+console.log(ritornoStringa);
+
+function stringaGirata(stringa) {
+    // trasformo una stringa in un array .SPLIT
+    // poi inverto l'ordine degli elementi .REVERSE
+    // trasformo l'array di una stringa e la ritorno .JOIN --> RETURN
+    return stringa.split('').reverse().join('')
+}
