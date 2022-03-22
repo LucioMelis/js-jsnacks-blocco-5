897faaae0c839d7a38035c875b232f8baadccb1b
luciomelis <luciomelis1992@gmail.com>
2022-03-22 19:07:22 +0100

    finish bonus 2

--- bonus/script.js
+++ bonus/script.js
@@ -22,3 +22,27 @@ function alternaArray() {
 
     return arrayCompleto
 }
+
+// BONUS 2
+// Scrivi una funzione che accetti tre argomenti: un array e due numeri 
+// (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
+// La funzione ritornerà un nuovo array
+// con i valori che hanno la posizione compresa tra “a” e “b”
+
+
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const nuovoArray = functionReturnArray(array, 2, 10);
+console.log(nuovoArray);
+
+function functionReturnArray(arrayX, num1, num2) {
+
+    const arreyNuovo = [];
+    for (let i = num1; i < (num2 - 1); i++) {
+        arreyNuovo.push(arrayX[i]);
+    }
+
+    return arreyNuovo
+}
+
